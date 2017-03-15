01c31829b84eed12107e413e9c645c6f264f69d8
Henry Kucab <kucab2345@gmail.com>
2017-03-14 19:11:44 -0600

    Removed comment

--- src/phase2/StatPopularTH.java
+++ src/phase2/StatPopularTH.java
@@ -11,19 +11,6 @@ public class StatPopularTH
 	public boolean displayHighestRatedTH(int limit, Statement stmt)
 	{
 		
-		/*
-		 * This will return the max starRating per thid, but not Per category. For that we need to cross reference
-		 * the feedback thid with the userhousing thid and group by category, but I couldnt not get it to work in workbench.
-		 * 
-		 * IT SHOULD RETURN THE HIGHEST RATED THID FOR EACH CATEGORY, HIGHEST RATING DENOTED BY AVG STAR RATING
-		 * 
-		 * SELECT F.thid, MAX(F.starRating) from feedback F
-			group by F.thid
-			
-		 * Try to edit this if you'd like. plzfixfeifei
-		 */
-		//sqlStatement = //????
-		
 		sqlStatement = "Select f.thid, t.category, AVG(f.starRating) from feedback f, temphousing t where (f.thid = t.thid and t.category = ANY(select category from temphousing)) group by f.thid order by AVG(f.starRating) DESC;";
 		try 
 		{
