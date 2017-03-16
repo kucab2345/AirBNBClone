17203b9ca332658b891e4d3e25cf31349e5ea203
Chandler Waugh <chandler@gbpackerfans.com>
2017-03-15 18:40:17 -0600

    Believe I have added and completed degrees of separation. Further testing is required.

--- src/phase2/DegreesOfSeparation.java
+++ src/phase2/DegreesOfSeparation.java
@@ -31,9 +31,19 @@ public class DegreesOfSeparation
 				}
 			}
 			
-			sqlStatement = "";  //TODO: Find users that are 1 degree away from the two given, then if there exists one they are two apart otherwise return -1 as they aren't close enough
+			sqlStatement = "SELECT COUNT(*) FROM favorites f3, favorites f4 WHERE f3.thid = f4.thid AND f3.login = \"" + user2 + "\" AND f4.login = ANY(SELECT DISTINCT(f2.login) FROM favorites f1, favorites f2 WHERE f1.thid = f2.thid AND f1.login = \"" + user1 + "\" AND f2.login = ANY(SELECT login FROM favorites WHERE login != \"" + user1 + "\" AND login != \"" + user2 + "\"));";
+ //TODO: Find users that are 1 degree away from the two given, then if there exists one they are two apart otherwise return -1 as they aren't close enough
 			Statement statement2 = statement.getConnection().createStatement();
 			result = statement2.executeQuery(sqlStatement);
+			while(result.next())
+			{
+				if(result.getInt(1) > 0)
+				{
+					return 2;
+				}
+			}
+			
+			return -1;
 		}
 		catch(Exception e)
 		{
